360b8df099b9b2dafdf09fb0c97360860de3acf7
João Lucas <johnascimentom@hotmail.com>
2026-02-02 15:02:04 -0300

    Funcao de criptografar mensagem usando uma chave

--- cripto_sai/criptografia.py
+++ cripto_sai/criptografia.py
@@ -1,17 +1,30 @@
 import random
 
 def GEN(seed):
-    # O tamanho da chave deve ser 4 * o tamanho da semente
-    # Se a semente for string, usamos o comprimento dela
+    # O tamanho da chave deve ser 4x o tamanho da semente
     tamanho_chave = len(seed) * 4
     
-    # Define a semente para o gerador ser "determinístico"
-    # Isso garante que a mesma semente gere a mesma chave
+    # Define a semente para o gerador ser determinístico
     random.seed(seed)
     
     # Gera uma lista de 0s e 1s
     chave = [random.randint(0, 1) for _ in range(tamanho_chave)]
-    print("Chave gerada:", chave)
+    print("Chave gerada: ", chave)
     return chave
 
-print(GEN("seed"))
+
+def ENC(K, M):
+    # Verificação de segurança: K e M devem ter o mesmo tamanho
+    if len(K) != len(M):
+        raise ValueError("A chave e a mensagem precisam ter o mesmo tamanho! Mensagem tem que ser 4x maior que a seed.")
+    
+    # Lógica de XOR entre a chave K e a mensagem M
+    cifra = []
+    for i in range(len(K)):
+        bit_cifra = K[i] ^ M[i] # Operação XOR
+        cifra.append(bit_cifra)
+
+    print("Mensagem cifrada: ", cifra)
+    return cifra
+
+ENC(GEN("s"), [1, 1, 1, 1])
